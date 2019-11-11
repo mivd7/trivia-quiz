b21a70ffd287c2d8ef84a794cff10d05389ac4c2
MIVD <mi.vandeurzen@gmail.com>
2019-11-11 08:16:18 +0100

    keep score

--- src/store/modules/question.js
+++ src/store/modules/question.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
-import { shuffle } from '../../lib/quizLogic';
+import { shuffle, checkAnswer } from '../../lib/quizLogic';
 
 const state = {
     questions: [],
     question: {},
     answers: [],
-    correctAnswer: ''
+    correctAnswer: '',
+    correctAmount: 0
 };
   
 const getters = {
     allQuestions: state => state.questions,
     currentQuestion: state => state.question,
     allAnswers: state => state.answers,
-    correctAnswer: state => state.correctAnswer
+    correctAnswer: state => state.correctAnswer,
 }
 
 const actions = {
@@ -24,6 +25,14 @@ const actions = {
     nextQuestion({ commit }) {
         commit('setNextQuestion')
         commit('setAnswers')
+    },
+    compareAnswers({ commit }, chosenAnswer) {
+        commit('setQuestionResult', chosenAnswer)
+        commit('setNextQuestion')
+        commit('setAnswers')
+    },
+    finalResult({ commit }, user) {
+      commit('checkQuizResult')
     }
 }
   
@@ -37,8 +46,17 @@ const mutations = {
         state.answers = shuffle(state.question.incorrect_answers),
         state.correctAnswer = state.question.correct_answer
     ),
-    setNextQuestion: (state) => state.question = state.questions.shift()
+    setQuestionResult: (state, chosenAnswer) => {
+        if (checkAnswer(chosenAnswer, state.correctAnswer)) {
+          state.correctAmount += 1
+        } else {
+          return;
+        }
+    },
+    setQuizResult: (state, )
+    setNextQuestion: (state) => state.question = state.questions.shift(),
 }
+  
   
 export default {
     state,
