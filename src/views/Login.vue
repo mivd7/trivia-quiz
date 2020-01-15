00022e30a161519a12d503d99a55a8721c9f068b
mivd7 <mi.vandeurzen@gmail.com>
2020-01-15 17:43:36 +0100

    type=button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <form>
       <input v-model="formModel.email" placeholder="email" label="enter your email"/><br/>
       <input v-model="formModel.password" placeholder="password" label="enter your password" type="password"/><br/>
-      <button @click="handleClick">Login</button>
+      <button type="button" @click="handleClick">Login</button>
       <p v-if="loginFailed" style="color: red;">Login failed! Please supply a username or wait until Max makes the signup functionality</p>
   </form>
 </template>
